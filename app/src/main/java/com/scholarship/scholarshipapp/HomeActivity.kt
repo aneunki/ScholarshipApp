be030d41612a7a35a5a70828cc347ea767f3fcd2
aneunki <dksdmsrl1005@naver.com>
2023-12-19 07:35:26 +0900

    scholarship ListView Design

--- app/src/main/java/com/scholarship/scholarshipapp/HomeActivity.kt
+++ app/src/main/java/com/scholarship/scholarshipapp/HomeActivity.kt
@@ -1,19 +1,25 @@
 package com.scholarship.scholarshipapp
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    private var scholarshipList = arrayListOf<Scholarship>(
+        Scholarship(1, "우리문화 장학재단", "조회수", -2),
+        Scholarship(2, "2023년도 서울", "조회수", 1),
+        Scholarship(3, "푸른등대", "조회수", 0)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val scholarshipListView: ListView = findViewById(R.id.scholarshipListView)
+
+        val scholarshipAdapter = ScholarshipAdapter(this, scholarshipList)
+        scholarshipListView.adapter = scholarshipAdapter
+
     }
-}
-
-class ScholarshipData(
-    val id: Int,
-    val title: String,
-    val d_day: Int
-) {
-
 }
